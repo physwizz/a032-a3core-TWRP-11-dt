17c16e9bf324e3b4851b3cfb1f231dd042dabaf2
physwizz <pollockg86@gmail.com>
2024-01-08 19:54:37 +1100

    first

---
+++ twrp_a3core.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Inherit from a3core device
+$(call inherit-product, device/samsung/a3core/device.mk)
+
+PRODUCT_DEVICE := a3core
+PRODUCT_NAME := twrp_a3core
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A032F
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="a3corexx-user 11 RP1A.201005.001 A032FXXU2AVH4 release-keys"
+
+BUILD_FINGERPRINT := samsung/a3corexx/a3core:11/RP1A.201005.001/A032FXXU2AVH4:user/release-keys
